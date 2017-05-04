10300a8a9fa1946eb28535d0641db61fd8e14230
Abdullah Almsaeed <almasaeed2010@gmail.com>
2017-05-04 15:45:46 -0400

    Initial release

---
+++ theme/templates/tripal_feature_jbrowse_pane.tpl.php
@@ -0,0 +1,10 @@
+<div id="tripal_jbrowse_pane_container">
+    <p>
+      <?php print $record->description ?>
+    </p>
+    <p>
+        <a href="<?php print $record->link ?>" class="btn btn-primary">
+            Go to JBrowse
+        </a>
+    </p>
+</div>
