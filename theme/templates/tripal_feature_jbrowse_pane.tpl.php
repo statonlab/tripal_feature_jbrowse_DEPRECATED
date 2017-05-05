0b7224ca0f42f17e6f5cc8b15621413f77adff72
Abdullah Almsaeed <almasaeed2010@gmail.com>
2017-05-05 13:06:33 -0400

    Enable text format

--- theme/templates/tripal_feature_jbrowse_pane.tpl.php
+++ theme/templates/tripal_feature_jbrowse_pane.tpl.php
@@ -1,6 +1,9 @@
+<?php
+$description = json_decode($record->description);
+?>
 <div id="tripal_jbrowse_pane_container">
     <p>
-      <?php print $record->description ?>
+      <?php print check_markup($description->value, $description->format) ?>
     </p>
     <p>
         <a href="<?php print $record->link ?>" class="btn btn-primary">
