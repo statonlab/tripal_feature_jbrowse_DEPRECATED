df152ca183b692482e10f943cf18fc72004150a2
Abdullah Almsaeed <almasaeed2010@gmail.com>
2017-05-05 11:34:03 -0400

    Deal with empty lists

--- theme/templates/tripal_feature_jbrowse_admin.tpl.php
+++ theme/templates/tripal_feature_jbrowse_admin.tpl.php
@@ -1,5 +1,10 @@
 <?php
 
-if($has_records) {
-    print $list_table;
-}
+if ($has_records):
+  print $list_table;
+else:
+  ?>
+  <p>
+    There are no records to display yet. You can add a new association by clicking the "Add" link above.
+  </p>
+<?php endif; ?>
